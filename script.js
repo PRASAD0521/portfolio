c1d1de80a524afb52c68da606f1f13861cde7a30
PRASAD0521 <satyadurgavaraprasadboddapu@gmail.com>
2024-07-24 15:14:37 +0530

    updated js

--- script.js
+++ script.js
@@ -19,18 +19,15 @@ gsap.to(".card",{
     opacity:1,
 })
 // ----------------------effects on page2--------------------- 
-// Function to handle animations and color changes with linear gradient
 function handleAnimations() {
     const projects = document.querySelectorAll(".project1");
-
-    // Define a list of gradients
     const gradients = [
-        "linear-gradient(to left, #000000, #4d2646)", // Example gradients
+        "linear-gradient(to left, #000000, #4d2646)", 
         "linear-gradient(to left, #000, #323663)",
         "linear-gradient(to left, #000, #4d343b)",
         "linear-gradient(to left, #000, #332562)",
         "linear-gradient(to left, #000, #204163)"
-    ]; // Add more gradients as needed
+    ];
 
     projects.forEach((project, index) => {
         const sno = document.querySelector(`#sn${index + 1}`);
@@ -39,36 +36,29 @@ function handleAnimations() {
             const projectRect = project.getBoundingClientRect();
             const triggerOffsetTop = window.innerHeight * 0.6;
             const triggerOffsetBottom = window.innerHeight * 0.6;
-
-            // Check if the project element is within the viewport
             if (
                 projectRect.top + triggerOffsetTop < window.innerHeight &&
                 projectRect.bottom - triggerOffsetBottom > 0
             ) {
-                // Animate x position and change background with gradient
                 gsap.to(sno, {
                     x: 80,
                     duration: 0.5,
-                    background: gradients[index % gradients.length], // Cycle through gradients
+                    background: gradients[index % gradients.length], 
                 });
             } else {
-                // Reset x position and background
                 gsap.to(sno, {
                     x: 0,
                     duration: 0.5,
-                    background: "initial", // Reset to initial background or none
+                    background: "initial", 
                 });
             }
         }
     });
 }
-
-// Event listener for scroll events with debounce
 document.addEventListener("scroll", () => {
     handleAnimations();
 });
 
-// Initial call to handleAnimations on DOMContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
     handleAnimations();
 });
